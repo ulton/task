3ae0cc6529043db53e23057c841c48ed071c7024
Wellington <wsa.jpb@gmail.com>
2021-02-06 17:07:05 -0300

    Initial commit

---
+++ src/test/java/com/go/task/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
